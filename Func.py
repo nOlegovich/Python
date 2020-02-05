13689a65001183684ce114522badc9a64af23edd
Nazariy <moroz.nazariy.fox@gmail.com>
2020-02-05 20:19:17 +0200

    First commit

--- Func.py
+++ Func.py
@@ -3,6 +3,7 @@ from random import randint as mrz
 a = (int(input("Введіть кількість елементів у списку: ")))
 b = (int(input("Введіть  значення, яке повинно бути максимальне в списку: ")))
 
+
 def fun (a,b):
     list = []
     for i in range(a):
